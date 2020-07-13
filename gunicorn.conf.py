bdf11797ecbb5b79fd8c0550350ec3861ef2e56b
Nikki <nikkiweat@gmail.com>
2020-07-13 23:28:13 +0800

    worker option exceeding heroku mem quota

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,6 @@
 import os
-import multiprocessing
 
 port = int(os.environ.get("PORT", 5000))
 host = '0.0.0.0'
 worker_class = 'gevent'  # necessary
 timeout = 90  # not necessary
-workers = multiprocessing.cpu_count() * 2 + 1
