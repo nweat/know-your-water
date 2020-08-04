2f370aac10d7df50f66ef897e7a6192afa977ff2
Nikki <nikkiweat@gmail.com>
2020-08-04 12:27:58 +0800

    add pollution sources reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,13 @@ import { riverStationsReducer, damStationsReducer } from './stationsReducer';
 import { visibilityFilterReducer } from '../reducers/visibilityFilterReducer';
 import { defaultLocationReducer } from '../reducers/defaultLocationReducer';
 import { loadingStatusReducer } from '../reducers/loadingStatusReducer';
+import { pollutionSourcesReducer } from '../reducers/pollutionSourcesReducer';
 
 export default combineReducers({
   river_stations: riverStationsReducer,
   dam_stations: damStationsReducer,
   visibility: visibilityFilterReducer,
   default_location: defaultLocationReducer,
-  loading: loadingStatusReducer
+  loading: loadingStatusReducer,
+  pollution_sources: pollutionSourcesReducer
 });
